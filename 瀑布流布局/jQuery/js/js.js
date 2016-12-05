c82bb24b349dc2fc28117db4782c4fde3362e3b3
mingwen <mingwen72@gmail.com>
2016-12-05 10:20:20 +0800

    jq瀑布流完成

--- 瀑布流布局/jQuery/js/js.js
+++ 瀑布流布局/jQuery/js/js.js
@@ -3,7 +3,21 @@
  */
 $(window).on('load',function () {
     waterFall();
-    $(window).on('')
+    var dataInt={'data':[{'src':'1.jpg'},{'src':'2.jpg'},{'src':'3.jpg'},{'src':'4.jpg'}]};
+    $(window).scroll(function(){
+        if (checkScrollSlide()){
+            $.each(dataInt.data,function (key,value) {
+                // console.log(value);
+                var $oBox = $('<div>').addClass('pin').appendTo($('#main'));
+                var $oPic = $('<div>').addClass('box').appendTo($oBox);
+                $('<img>').attr('src','./images/'+$(value).attr('src')).appendTo($oPic);
+            });
+            waterFall();
+        }
+    });
+
+
+
 });
 
 function waterFall() {
@@ -28,4 +42,12 @@ function waterFall() {
         }
     });
 // console.log(hArr)
+}
+
+function checkScrollSlide() {
+    var $lastBox = $('#main>div').last();
+    var lastBoxDis = $lastBox.offset().top+Math.floor($lastBox.outerHeight()/2);
+    var scrollTop = $(window).scrollTop();
+    var documentH = $(window).height();
+    return (lastBoxDis<scrollTop+documentH)?true:false;
 }
