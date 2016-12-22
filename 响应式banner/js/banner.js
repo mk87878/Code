6e6b43354b3ce3b384d26982627d7cb17505b275
mingwen <mingwen72@gmail.com>
2016-12-22 14:52:47 +0800

    响应式banner-应对 ctrl+滚轮 缩放网页变形问题

--- 响应式banner/js/banner.js
+++ 响应式banner/js/banner.js
@@ -1,3 +1,35 @@
 /**
  * Created by 张铭纹 on 2016/12/22.
  */
+//页面初始化
+$(function(){
+    resizeImg();
+});
+//窗口变化时
+$(window).resize(function(){
+    resizeImg();
+});
+/*首页banner高度*/
+function resizeImg() {
+    var imgWidth = 1920;
+    var imgHeight = 840;//定义图片默认宽高
+    var winWidth = $(window).width();
+    var winHeight = $(window).height() - $(".box").height();
+    var widthRatio = winWidth / imgWidth;//window宽度与图片宽度之比
+    var heightRatio = winHeight / imgHeight;//window高度与图片高度之比
+    var widthDiff = heightRatio * imgWidth;
+    var heightDiff = widthRatio * imgHeight;
+    $(".banner").height(winHeight);
+
+    if (heightDiff > winHeight) {
+        $("#bannerImg").css({
+            width: '100%',
+            height: heightDiff + 'px'
+        });
+    } else {
+        $("#bannerImg").css({
+            width: widthDiff + 'px',
+            height: winHeight + 'px'
+        });
+    }
+}
