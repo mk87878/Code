b360287cf200cdef0143fe0f43ade712c71c095f
mingwen <mingwen72@gmail.com>
2016-12-29 11:07:36 +0800

    图片预加载

--- jQuery插件学习/图片预加载插件/jquery.loading.js
+++ jQuery插件学习/图片预加载插件/jquery.loading.js
@@ -7,10 +7,11 @@ jQuery.fn.beforeload= function(options) {
     }, options);
     var self = this;
     self.hide();
-    var img = new Image();
+    var img = new Image();//创建一个img对象 等同于$('<img>')
+    // console.log(img);
     $(img).load(function() {
         self.attr("src", options.src);
         self.fadeIn("slow");
     }).attr("src", options.src);
     return self;
-}
+};
